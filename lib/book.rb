74776c49a8873e91bc7c1607adcd84292938f30b
Varvara Babkova <varvara.munday@gmail.com>
2019-06-27 13:11:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  GENRES = ["Thriller", "Science Fiction", "Romance"]
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,6 +10,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
